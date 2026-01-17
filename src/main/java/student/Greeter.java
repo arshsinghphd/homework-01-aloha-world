b357440d65310a06b71fb1705fa36e6b35f1ef9f
arshsinghphd <57355255+arshsinghphd@users.noreply.github.com>
2026-01-17 11:50:23 -0800

    removing extra spaces

--- src/main/java/student/Greeter.java
+++ src/main/java/student/Greeter.java
@@ -42,7 +42,6 @@ public class Greeter {
      * This is the constructor for the Greeter class.
      *
      * This constructor assumes 2 as the default locality.
-     * 
      * @param name of the person to greet
      */
     public Greeter(String name) {
@@ -82,7 +81,6 @@ public class Greeter {
      *
      * This is called an "accessor" method. It is a method that returns the value of a private
      * field. It is a good practice to make fields private and use accessors to get the value.
-     * 
      * @return the int value of the locality
      */
     public int getLocality() {
@@ -115,7 +113,6 @@ public class Greeter {
      * This is called an "overloaded" method. It is a method with the same name as another method,
      * but with different parameters. This helps simplify the code calls, and allows for more
      * flexibility in the code.
-     * 
      * @return the greeting
      * @see #greet(boolean)
      */
@@ -186,7 +183,6 @@ public class Greeter {
     /**
      * Returns the locality as a string. If the locality is not between 1 and localityList.size() it
      * will return "USA"
-     * 
      * @return the locality as a string
      */
     private String getLocalityString() {
@@ -205,7 +201,6 @@ public class Greeter {
      * hashcode will be the same.
      *
      * Java assumes that if .equals is true, the hashcodes should also be the same.
-     * 
      * @return the hashcode
      */
     @Override
@@ -262,7 +257,6 @@ public class Greeter {
 
     /**
      * Returns a copy of the locality list.
-     * 
      * @return a copy of the locality list
      */
     public static List<String> getLocalityList() {
