c08c043d879e3fb127033bb8347eb5d7c2170a37
arshsinghphd <57355255+arshsinghphd@users.noreply.github.com>
2026-01-17 11:28:20 -0800

    removed some extra spaces

--- src/main/java/student/AlohaWorld.java
+++ src/main/java/student/AlohaWorld.java
@@ -45,15 +45,12 @@ public final class AlohaWorld {
     public static void main(String[] args) {
         String name = ConsoleView.getName();
         int locality = ConsoleView.getLocality();
-
         Greeter greeter = new Greeter(name, locality);
         ConsoleView.printGreeting(greeter.greet());
-        
         while (ConsoleView.checkRunAgain()) {
             locality = ConsoleView.getLocality();
             greeter.setLocality(locality);
             ConsoleView.printGreeting(greeter.greet());
         }
-
     }
 }
